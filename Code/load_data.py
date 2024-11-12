16c99086713fdcec49ddb51dfa064c7ad330c3f8
Jonah Weigand-Whittier <jweigandwhittier@berkeley.edu>
2024-11-12 15:20:10 -0800

    Added progress bar

--- Code/load_data.py
+++ Code/load_data.py
@@ -8,6 +8,7 @@ Created on Tue Nov 12 14:12:35 2024
 import torch
 import esm
 import pandas as pd
+from tqdm import tqdm
 
 # Path to data folder
 directory = '../GATSol/dataset/'
@@ -23,17 +24,16 @@ model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
 batch_converter = alphabet.get_batch_converter()
 model.eval()  # Disable dropout for deterministic results
 
-# Convert the sequences to batch format
-batch_labels, batch_strs, batch_tokens = batch_converter(sequences_train)
+# Prepare a list to store embeddings
+embeddings_list = []
 
-# Generate embeddings
-with torch.no_grad():
-    results = model(batch_tokens, repr_layers=[33])
-embeddings = results["representations"][33]
+# Loop through each sequence with tqdm to show progress
+for batch_labels, batch_strs, batch_tokens in tqdm([batch_converter([seq]) for seq in sequences_train], desc="Generating Embeddings"):
+    with torch.no_grad():
+        results = model(batch_tokens, repr_layers=[33])
+    embedding = results["representations"][33].mean(dim=1)  # Mean pooling
+    embeddings_list.append(embedding.cpu().numpy())
 
-# Aggregate embeddings by averaging across sequence length
-sequence_embeddings = embeddings.mean(dim=1)
-
-# Save embeddings along with gene names and solubility values for downstream use
-df_train['embedding'] = [embedding.cpu().numpy() for embedding in sequence_embeddings]
-df_train.to_pickle(directory + "eSol_train_embeddings.pkl")  # Save as a pickle file for easy loading later
+# Add embeddings to the DataFrame and save
+df_train['embedding'] = embeddings_list
+df_train.to_pickle(directory + 'eSol_train.pkl')  # Save with embeddings
